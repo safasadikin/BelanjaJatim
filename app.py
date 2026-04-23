766eaa6de0c503f2d9bcf799287d19b33fc95eb4
Rakha Safa <rakhareno290604@gmail.com>
2026-04-23 13:45:51 +0700

    revisi aplikasi

--- app.py
+++ app.py
@@ -307,7 +307,7 @@ def show_auth_page():
         .stTabs [data-baseweb="tab"] {{ font-weight:700 !important; font-size:1rem !important; }}
         /* Logo turun pakai padding-top */
         .login-logo-wrap {{ display:flex; align-items:center; justify-content:center; padding-top:280px; }}
-        .login-logo-img {{ width:220px; pointer-events:none; filter:drop-shadow(0 4px 20px rgba(0,0,0,0.5)); }}
+        .login-logo-img {{ width:280px; pointer-events:none; filter:drop-shadow(0 4px 20px rgba(0,0,0,0.5)); }}
         /* Form wrapper */
         .login-form-wrap {{ padding-top:60px; }}
         .login-title {{ font-size:2.1rem !important; font-weight:900 !important; color:white !important;
